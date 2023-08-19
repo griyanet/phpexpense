4bb5ac160e94761185564805e3f791b5d16dea2b
Trisna Widia <griyanetcafe@gmail.com>
2023-08-19 14:49:07 +0800

    - add templating clas - add the view

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -8,12 +8,13 @@ class Router
 {
     private array $routes = [];
 
-    public function add(string $method, string $path): void
+    public function add(string $method, string $path, array $controller): void
     {
         $path = $this->normalizePath($path);
         $this->routes[] = [
             'path' => $path,
-            'method' => strtoupper($method)
+            'method' => strtoupper($method),
+            'controller' => $controller
         ];
     }
 
@@ -22,5 +23,22 @@ class Router
         $path = trim($path, '/');
         $path = "/{$path}/";
         return preg_replace('#[/]{2,}#', '/', $path);
+    }
+
+    public function dispatch(string $path, string $method)
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+                continue;
+            }
+
+            [$class, $function] = $route['controller'];
+            //dd($class);
+            $controllerInstance = new $class;
+            $controllerInstance->{$function}();
+        }
     }
 }
---
+++ src/Framework/TemplateEngine.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework;
+
+class TemplateEngine
+{
+    public function __construct(private string $basePath)
+    {
+    }
+
+    public function render(string $template, array $data = [])
+    {
+        extract($data, EXTR_SKIP);
+        ob_start();
+
+        include "{$this->basePath}/{$template}";
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}

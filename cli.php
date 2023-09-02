ddcba00ab22d275afa3058754ec13f30e0c9445f
Trisna Widia <griyanetcafe@gmail.com>
2023-09-02 18:11:12 +0800

    - create cli for migration

---
+++ cli.php
@@ -0,0 +1,16 @@
+<?php
+
+$driver = 'mysql';
+$config = http_build_query( data: [
+	'host' => 'localhost',
+	'port' => '3306',
+	'dbname' => 'phpiggy'
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+$username = 'root';
+$password = '';
+
+$db = new PDO($dsn, $username, $password);
+
+echo "connected to database";
